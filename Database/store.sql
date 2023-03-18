52b17ede381e89d6fcaed743a28e75ecb4002cb9
Karunagaran2308 <128219116+Karunagaran2308@users.noreply.github.com>
2023-03-18 14:58:13 +0530

    Add files via upload

---
+++ Database/store.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `username` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `mobnum` varchar(222) NOT NULL,
+  `password1` varchar(222) NOT NULL,
+  `password2` varchar(222) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
